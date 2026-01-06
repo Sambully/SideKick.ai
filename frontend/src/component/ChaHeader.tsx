9362b403a3594e645cfbe607d72d1e876ea5da9a
Samarth Pal <samarthpal1912005@gmail.com>
2026-01-06 11:59:08 +0530

    chat header drop down working

--- frontend/src/component/ChaHeader.tsx
+++ frontend/src/component/ChaHeader.tsx
@@ -2,8 +2,10 @@ import { Button } from "@/components/ui/button";
 import { ChevronLeft, Delete, DeleteIcon, Edit, LucideDelete, MessagesSquare, MoreVertical, Trash } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { useUser } from "@clerk/clerk-react";
+import { useUser, useAuth } from "@clerk/clerk-react";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { toast } from "sonner";
+import axios from "axios";
 
 interface Companion {
     id: string,
@@ -32,9 +34,29 @@ interface Message {
 export const ChatHeader = ({ companion }: { companion: Companion | null }) => {
     const navigation = useNavigate();
     const { user } = useUser();
+    const { getToken } = useAuth();
+
+    const ondelete = async () => {
+        try {
+            const token = await getToken();
+            await axios.delete(`http://localhost:3000/companion/${companion?.id}`, {
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                }
+            });
+            toast.success("Companion deleted successfully");
+            navigation("/dashboard");
+
+        } catch (err) {
+            console.error("Delete Error:", err)
+            toast.error("Error in deleting companion", {
+                description: "Please try again"
+            })
+        }
+    }
     return (
-        <div className="border-b flex felx-col justify-between w-full border-primary/25 p-4">
-            <div className="felx gap-x-2 items-center font-bold">
+        <div className="border-b flex justify-between w-full border-primary/25 p-4">
+            <div className="gap-x-2 items-center font-bold">
                 <Button size="icon" variant="ghost" onClick={() => navigation("/dashboard")}>
                     <ChevronLeft className="h-24 w-24 stroke-[4]" />
                 </Button>
@@ -62,7 +84,7 @@ export const ChatHeader = ({ companion }: { companion: Companion | null }) => {
                             <Edit className="w-4 h-4 mr-2" />
                             Edit
                         </DropdownMenuItem>
-                        <DropdownMenuItem className="text-red-400">
+                        <DropdownMenuItem onClick={ondelete} className="text-red-400">
                             <Trash className="w-4 h-4 mr-2" />
                             Delete
                         </DropdownMenuItem>
